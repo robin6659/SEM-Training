cea5b94de34c0e5125dc7f5ecb2646bacffd1fa6
Robin Thannickal Abraham <118527622+robin6659@users.noreply.github.com>
2025-04-02 15:50:24 +0530

    Boolean exp

---
+++ SEM-ForStudy/src/main/java/selflearn/BooleanExp.java
@@ -0,0 +1,22 @@
+package selflearn;
+
+import java.util.Scanner;
+
+public class BooleanExp {
+	public static void voting() {
+
+		int votingAge = 18;
+		
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter age");
+		int myAge = scanner.nextInt();
+
+		System.out.println(myAge>=votingAge);
+
+	}
+
+	public static void main (String[] args) {
+		voting();
+	}
+}
