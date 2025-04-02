ea09e3d0546ad98afc8849557f19751ba5bd78b5
Robin Thannickal Abraham <118527622+robin6659@users.noreply.github.com>
2025-04-02 11:43:25 +0530

    printing pgms

---
+++ SEM-ForStudy/src/main/java/basicprinting/Car.java
@@ -0,0 +1,19 @@
+package basicprinting;
+
+public class Car {
+	public void carFeature() {
+		String make = "Porsche";
+		String model = "911";
+		String color= "Crayon";
+
+		System.out.println("Make of the car is "+make+" and the model is "+model+" wrapped with "+color);
+
+	}
+
+	public static void main (String[] args) {
+
+		Car car = new Car();
+		car.carFeature();
+		//carFeature();
+	}
+}
---
+++ SEM-ForStudy/src/main/java/basicprinting/Name.java
@@ -0,0 +1,13 @@
+package basicprinting;
+
+public class Name {
+
+	public static void namePrint() {
+		String nameofthecar = "Porsche";
+		System.out.println("Name of the cars is "+nameofthecar);
+	}
+
+	public static void main (String[] args) {
+		namePrint();
+	}
+}
