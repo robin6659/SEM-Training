b45cdcee53409187c54108b05d6aaf88ed407859
Robin Thannickal Abraham <118527622+robin6659@users.noreply.github.com>
2025-04-07 11:20:17 +0530

    method overloading

---
+++ SEM-ForStudy/src/main/java/Polymorphism/CompiletimeProg1.java
@@ -0,0 +1,30 @@
+package Polymorphism;
+
+public class CompiletimeProg1 {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static int add(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public static int multiply(int a, int b, int c) {
+		return a * b * c;
+	}
+
+	public static void main (String[] args) {
+
+		System.out.println("Add two numbers" +add(10,40));
+		System.out.println("Add three numbers"+add(5,3,2));
+
+		System.out.println("Multiply two numbers"+multiply(3,2));
+		System.out.println("Multiply three numbers"+multiply(3,3,1));
+	}
+
+}
