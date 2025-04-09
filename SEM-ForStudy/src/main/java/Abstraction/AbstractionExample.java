ebd8c53e24f0638d73aa20e37473a483887338ae
Robin Thannickal Abraham <118527622+robin6659@users.noreply.github.com>
2025-04-09 11:16:13 +0530

    abs

--- SEM-ForStudy/src/main/java/Abstraction/AbstractionExample.java
+++ SEM-ForStudy/src/main/java/Abstraction/AbstractionExample.java
@@ -5,6 +5,6 @@ public abstract class AbstractionExample {
     public abstract void displayMessage();
 
     public void showInfo() {
-        System.out.println("Hi, this is a normal method from Abstract class.");
+        System.out.println("Normal method from Abstract");
     }
 }
