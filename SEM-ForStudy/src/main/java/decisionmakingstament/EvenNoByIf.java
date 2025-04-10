25fa34cb2511d0b98a4cdf8d538a1b5a4a7aa513
Robin Thannickal Abraham <118527622+robin6659@users.noreply.github.com>
2025-04-10 15:19:00 +0530

    if

---
+++ SEM-ForStudy/src/main/java/decisionmakingstament/EvenNoByIf.java
@@ -0,0 +1,13 @@
+package decisionmakingstament;
+
+public class EvenNoByIf {
+
+	public static void main(String[] args) {
+
+		int num = 8;
+
+		if (num % 2 == 0) {
+			System.out.println(num + " is Even Number");
+		}
+	}
+}
---
+++ SEM-ForStudy/src/main/java/decisionmakingstament/PositiveNoByIf.java
@@ -0,0 +1,13 @@
+package decisionmakingstament;
+
+public class PositiveNoByIf {
+
+	public static void main(String[] args) {
+
+		int num = 10;
+
+		if (num < 0) {
+			System.out.println(num + " is a Positive Number");
+		}
+	}
+}
