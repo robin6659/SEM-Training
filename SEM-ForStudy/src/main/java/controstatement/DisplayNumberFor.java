228bcdb67e7be893bb1f6f5c40cee3d9648b77d6
Robin Thannickal Abraham <118527622+robin6659@users.noreply.github.com>
2025-04-11 10:35:23 +0530

    For loop

---
+++ SEM-ForStudy/src/main/java/controstatement/DisplayNumberFor.java
@@ -0,0 +1,14 @@
+package controstatement;
+
+public class DisplayNumberFor {
+
+	public static void main (String[] args) {
+		int num = 5;
+
+		for (int n = 1; n <= num; n++) {
+
+			System.out.println(n);
+
+		}
+	}
+}
---
+++ SEM-ForStudy/src/main/java/controstatement/DisplayTextFor.java
@@ -0,0 +1,14 @@
+package controstatement;
+
+public class DisplayTextFor {
+
+	public static void main (String[] args) {
+		int num = 5;
+
+		for (int n = 1; n <= num; n++) {
+
+			System.out.println("Java is fun");
+
+		}
+	}
+}
