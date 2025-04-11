caf641dedc2174202819ea42804f0130343afd88
Robin Thannickal Abraham <118527622+robin6659@users.noreply.github.com>
2025-04-11 11:14:13 +0530

    dowhile

---
+++ SEM-ForStudy/src/main/java/controlstatement/EvenDoWhile.java
@@ -0,0 +1,17 @@
+package controlstatement;
+
+public class EvenDoWhile {
+	
+	public static void main(String[] args) {
+		
+		int i = 2;
+
+		do {
+			
+			System.out.println(i);
+			
+			i = i + 2;
+			
+		} while(i <= 20);
+	}
+}
---
+++ SEM-ForStudy/src/main/java/controlstatement/MultipicationDoWhile.java
@@ -0,0 +1,16 @@
+package controlstatement;
+
+public class MultipicationDoWhile {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+
+		do {
+			System.out.println(2 * i);
+
+			i++;
+
+		} while(i <= 10);
+	}
+}
