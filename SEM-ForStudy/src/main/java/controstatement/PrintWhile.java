1554f55179d3ef7f69296fb350bb0d1f3e375015
Robin Thannickal Abraham <118527622+robin6659@users.noreply.github.com>
2025-04-11 10:59:15 +0530

    While Loop renamed

---
+++ SEM-ForStudy/src/main/java/controstatement/PrintWhile.java
@@ -0,0 +1,14 @@
+package controstatement;
+
+public class PrintWhile {
+	
+	public static void main(String[] args) {
+		
+		int i = 1;
+		while(i <= 10)/*checks condition initially*/ {
+			System.out.println(i);
+			i++;/*after printing, increments the value of i */
+		}
+	}
+}
+/*Entry controlled loop*/
