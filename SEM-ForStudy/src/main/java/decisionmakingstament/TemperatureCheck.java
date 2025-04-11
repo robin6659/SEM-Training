be1dc3e3dd75f15f5e809aaa6a427f308208b8b1
Robin Thannickal Abraham <118527622+robin6659@users.noreply.github.com>
2025-04-11 09:05:41 +0530

    simple if

--- SEM-ForStudy/src/main/java/decisionmakingstament/TemperatureCheck.java
+++ SEM-ForStudy/src/main/java/decisionmakingstament/TemperatureCheck.java
@@ -2,4 +2,12 @@ package decisionmakingstament;
 
 public class TemperatureCheck {
 
+	public static void main(String[] args) {
+
+		int temperature = 42;
+
+		if (temperature > 40) {
+			System.out.println("High Temperature");
+		}
+	}
 }
